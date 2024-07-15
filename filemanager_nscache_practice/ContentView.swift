9ea97c5dba93fe5069c1fe7354f6fb032082a157
imdung <dungmanh74@gmail.com>
2024-07-15 17:33:06 +0700

    Initial Commit

---
+++ filemanager_nscache_practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  filemanager_nscache_practice
+//
+//  Created by ImDung on 15/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ filemanager_nscache_practice/filemanager_nscache_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  filemanager_nscache_practiceApp.swift
+//  filemanager_nscache_practice
+//
+//  Created by ImDung on 15/7/24.
+//
+
+import SwiftUI
+
+@main
+struct filemanager_nscache_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
